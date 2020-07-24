29436f2dfa2c1fc39028860ebe20b9ce592d9d8e
tolstislon <tolstislon@gmail.com>
2020-07-24 18:58:54 +0300

    Updating patterns to libphonenumber v8.12.7

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@ import pytest
 from phone_gen.patterns import PATTERNS
 
 
+@pytest.mark.phonenumbers
 @pytest.mark.parametrize('country', random.sample(tuple(PATTERNS['data'].keys()), 20))
 def test_get_country(capfd, country):
     os.system('phone-gen {}'.format(country))
@@ -15,6 +16,7 @@ def test_get_country(capfd, country):
     assert phonenumbers.is_valid_number_for_region(num_obg, country)
 
 
+@pytest.mark.phonenumbers
 @pytest.mark.parametrize('country', random.sample(tuple(PATTERNS['data'].keys()), 20))
 def test_get_without_country_code(capfd, country):
     os.system('phone-gen {} -n'.format(country))
@@ -30,6 +32,7 @@ def test_invalid_country(capfd):
     assert captured.out.strip() == 'Error: Not found country "qwe"'
 
 
+@pytest.mark.phonenumbers
 @pytest.mark.parametrize('country_name, code', (('Germany', 'DE'), ('Panama', 'PA'), ('Turkey', 'TR')))
 def test_find_country_name(capfd, country_name, code):
     os.system('phone-gen {}'.format(country_name))
@@ -38,6 +41,7 @@ def test_find_country_name(capfd, country_name, code):
     assert phonenumbers.is_valid_number_for_region(num_obg, code)
 
 
+@pytest.mark.phonenumbers
 @pytest.mark.parametrize('iso, code', (('VNM', 'VN'), ('SLE', 'SL'), ('MCO', 'MC')))
 def test_find_iso3(capfd, iso, code):
     os.system('phone-gen {}'.format(iso))
