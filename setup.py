238be29c1fda0d6973879b7f67b89b76c1cb9645
tolstislon <tolstislon@gmail.com>
2020-09-02 21:17:23 +0300

    Updating patterns to libphonenumber v8.12.9

--- setup.py
+++ setup.py
@@ -2,42 +2,42 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-readme = Path('.', 'README.md').absolute()
-with readme.open('r', encoding='utf-8') as file:
+readme = Path(".", "README.md").absolute()
+with readme.open("r", encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
-    name='phone_gen',
-    packages=find_packages(exclude=('tests', 'dev_tools')),
-    url='https://github.com/tolstislon/phone-gen',
-    license='MIT License',
-    author='tolstislon',
-    author_email='tolstislon@gmail.com',
-    description='International phone number generation',
+    name="phone_gen",
+    packages=find_packages(exclude=("tests", "dev_tools")),
+    url="https://github.com/tolstislon/phone-gen",
+    license="MIT License",
+    author="tolstislon",
+    author_email="tolstislon@gmail.com",
+    description="International phone number generation",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     use_scm_version={"write_to": "phone_gen/__version__.py"},
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     entry_points={
-        'console_scripts': ['phone-gen=phone_gen.cli:main'],
+        "console_scripts": ["phone-gen=phone_gen.cli:main"],
     },
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     include_package_data=True,
-    keywords=['testing', 'test-data', 'phone-number', 'phone'],
+    keywords=["testing", "test-data", "phone-number", "phone"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Communications :: Telephony'
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Communications :: Telephony",
+    ],
 )
