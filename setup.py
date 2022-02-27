363604ad25a5643e69518c60972ad75820cb92f6
tolstislon <tolstislon@gmail.com>
2022-02-27 04:37:48 +0300

    Updating patterns to libphonenumber v8.12.44

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     description="International phone number generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='2.2.2',
+    version='2.2.3',
     entry_points={
         "console_scripts": ["phone-gen=phone_gen.cli:main"],
     },
