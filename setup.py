1638b9e5550b21b20c9ffb274fd03ee43d3cb22a
tolstislon <34726608+tolstislon@users.noreply.github.com>
2022-05-31 16:26:29 +0300

    Updating patterns to libphonenumber v8.12.49 (#59)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="International phone number generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version='2.2.7',
+    version='2.2.8',
     entry_points={
         "console_scripts": ["phone-gen=phone_gen.cli:main"],
     },
