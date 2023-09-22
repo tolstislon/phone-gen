538bb52e6f11e0ce940a87fd5c4b347054f32d78
tolstislon <34726608+tolstislon@users.noreply.github.com>
2023-09-22 19:25:54 +0300

    Fix setuptools_scm version (#97)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     use_scm_version={"write_to": "phone_gen/__version__.py"},
-    setup_requires=["setuptools_scm"],
+    setup_requires=["setuptools_scm==7.1.0"],
     entry_points={
         "console_scripts": ["phone-gen=phone_gen.cli:main"],
     },
